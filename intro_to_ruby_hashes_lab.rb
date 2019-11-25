22ffdef7e36e5c9def5571b9595b6e47a5bbc890
Marcel Rico <personal.mr4@gmail.com>
2019-11-25 23:10:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,5 +22,5 @@ def monopoly_with_fourth_tier
   new_has[:railroads][:names][:reading_railroad][:mortgage_value] = 100
   new_has[:railroads][:names][:pennsylvania_railroad][:mortgage_value] = 100
   new_has[:railroads][:names][:b_and_o_railroad][:mortgage_value] = 100
-  new_has[:railroads][:names][:reading_railroad][:mortgage_value] = 100
+  new_has[:railroads][:names][:shortline_railroad][:mortgage_value] = 100
 end
